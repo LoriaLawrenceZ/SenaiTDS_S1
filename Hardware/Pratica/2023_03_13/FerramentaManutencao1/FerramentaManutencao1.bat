95e81f84997a5183a1b6e523e3db384c1e082668
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-03-13 16:58:24 -0300

    Ferramenta de Melhoria 1 e 2 13/03/2023

---
+++ Hardware/Pratica/2023_03_13/FerramentaManutencao1/FerramentaManutencao1.bat
@@ -0,0 +1,72 @@
+@echo off
+cls
+:menu
+cls
+color 80
+
+date /t
+
+echo Computador: %computername%            Usuario: %username%
+
+echo            MENU TAREFAS
+echo  =================================
+echo * 1. Esvaziar Lixeira             *
+echo * 2. Fazer Backup                 *
+echo * 3. Escanear Disco Local         *
+echo * 4. Painel de Controle           *
+echo * 5. Sair                         *
+echo  =================================
+
+set /p opcao= Escolha uma opcao: 
+echo ---------------------------------
+if %opcao% equ 1 goto opcao1
+if %opcao% equ 2 goto opcao2
+if %opcao% equ 3 goto opcao3
+if %opcao% equ 4 goto opcao4
+if %opcao% equ 5 goto opcao5
+if %opcao% GEQ 6 goto opcao6
+
+:opcao1
+cls
+rd /S /Q C:\$Recicle.bin
+echo  =============================
+echo *      Lixeira Esvaziada      *
+echo  =============================
+pause
+goto menu
+
+:opcao2
+cls
+xcopy /T /C C:\Users\Aluno\Documents\*.*C:\Users\Aluno\Desktop
+echo  ==========================
+echo *     Backup Concluido     *
+echo  ==========================
+pause
+goto menu
+
+:opcao3
+cls
+echo  ===============================
+echo *     Escaneamento de Disco     *
+echo  ===============================
+chkdsk C:
+pause
+goto menu
+
+:opcao4
+cls
+control.exe
+pause
+goto menu
+
+:opcao5
+cls
+exit
+
+:opcao6
+cls
+echo  ================================================
+echo *    Opção Invalida! Escolha outra opção do menu *
+echo  ================================================
+pause
+goto menu
