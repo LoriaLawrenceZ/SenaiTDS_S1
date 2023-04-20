c087b5e1175219211a8494ca31ac138a7654b4ba
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-04-20 15:01:17 -0300

    exercícios população finalizados

--- Java/Pratica/2023_04/ExerciciosExtraForWhile/Exercicios.java
+++ Java/Pratica/2023_04/ExerciciosExtraForWhile/Exercicios.java
@@ -1,10 +1,9 @@
 package ExerciciosExtraForWhile;
 
 import java.util.Scanner;
-import java.lang.Math;
 
 public class Exercicios {
-    Scanner sc = new Scanner(System.in, "latin");
+    Scanner sc = new Scanner(System.in);
 
     public void userSenha(){
         String user, senha;
@@ -62,11 +61,78 @@ public class Exercicios {
         }
     }
 
-    public void populacao(){}
+    public void populacao(){
+        int paisA = 80000, paisB = 200000, anos = 0;
+        double taxaAcresc = 1.3, taxaBcresc = 1.15;
 
-    public void populacao2(){}
+        while (paisA < paisB) {
+            paisA *= taxaAcresc;
+            paisB *= taxaBcresc;
+            anos++;
+        }
 
-    public void numMaior(){}
+        System.out.println("Serão necessários ao menos "+ anos +" anos para que a população do país A ultrapasse ou iguale a do país B");
+    }
+
+    public void populacao2(){
+        int paisA, paisB, anos = 0;
+        double taxaAcresc, taxaBcresc;
+
+        System.out.println("Insira a população de um país A");
+        paisA = sc.nextInt();
+        while (paisA < 0) {
+            System.out.println("Insira um valor válido.");
+            paisA = sc.nextInt();
+        }
+
+        System.out.println("Insira a população de um país B");
+        paisB = sc.nextInt();
+        while (paisB < 0) {
+            System.out.println("Insira um valor válido.");
+            paisB = sc.nextInt();
+        }
+
+        System.out.println("Insira a taxa de crescimento do país A");
+        taxaAcresc = sc.nextInt();
+        while (taxaAcresc <= 0) {
+            System.out.println("Insira um valor válido.");
+            taxaAcresc = sc.nextInt();
+        }
+
+        System.out.println("Insira a taxa de crescimento do país B");
+        taxaBcresc = sc.nextInt();
+        while (taxaBcresc <= 0) {
+            System.out.println("Insira um valor válido.");
+            taxaBcresc = sc.nextInt();
+        }
+
+        if (paisA < paisB) {
+            while (paisA < paisB) {
+                paisA *= taxaAcresc;
+                paisB *= taxaBcresc;
+                anos++;
+            }
+            
+            System.out.println("Serão necessários ao menos "+ anos +" anos para que a população do país A ultrapasse ou iguale a do país B");
+        }
+        else if(paisA == paisB){
+            System.out.println("Os países já tem populações igualadas");
+        }
+        else {
+            while (paisB < paisA) {
+                paisA *= taxaAcresc;
+                paisB *= taxaBcresc;
+                anos++;
+            }
+            
+            System.out.println("Serão necessários ao menos "+ anos +" anos para que a população do país B ultrapasse ou iguale a do país A");
+        }
+
+    }
+
+    public void numMaior(){
+
+    }
 
     public void divisivel(){}
 
