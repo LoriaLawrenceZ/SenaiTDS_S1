404b09999fb1a8a66598abbe4456f285049e16f8
Lorenzo O. Zimbres <97912499+LoriaLawrenceZ@users.noreply.github.com>
2023-05-09 19:54:55 +0000

    Extras 1- 4 finalizados

---
+++ Java/Pratica/2023_05/ListaExtraMatriz/App.java
@@ -0,0 +1,60 @@
+package ListaExtraMatriz;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in, "latin1");
+        
+        Extras obj = new Extras();
+
+        String escolha = "";
+    
+        do
+        {
+            System.out.println(
+                    "\n------------------------------------------------------------------------------------\nEscolha qual exemplo deseja ver:\n[1] - Exercício 1\n[2] - Exercício 2\n[3] - Exercício 3\n[4] - Exercício 4\n[5] - Exercício 5\n[6] - Exercício 6\n[S] - SAIR\n[Apenas Números]");
+    
+            escolha = sc.next().toLowerCase();
+    
+            switch (escolha) {
+                  case "1":
+                        obj.Extra1();
+                        break;
+
+                  case "2":
+                        obj.Extra2();
+                        break;
+
+                  case "3":
+                        obj.Extra3();
+                        break;
+
+                  case "4":
+                        obj.Extra4();
+                        break;
+
+                  case "a":
+                        obj.Extra5();
+                        break;
+
+                  case "b":
+                        obj.Extra6();
+                        break;
+
+                  case "s":
+                        System.out.println("----====Encerrando Programa====----");
+                        escolha = "N";
+                        break;
+    
+                  default:
+                        System.out.println("Insira um valor válido da próxima vez");
+                        escolha = "N";
+                        break;
+            }
+    
+        }while(escolha!="N");
+
+        sc.close();
+    }
+}
---
+++ Java/Pratica/2023_05/ListaExtraMatriz/Extras.java
@@ -0,0 +1,178 @@
+package ListaExtraMatriz;
+
+import java.util.Random;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Extras {
+      Scanner sc = new Scanner(System.in, "latin1");
+      Random rd = new Random();
+
+      public void Extra1(){
+            int matriz[][] = new int[4][4];
+
+            //Dando valor
+            for (int i = 0; i < matriz.length; i++) {
+                  for (int j = 0; j < matriz[i].length; j++) {
+                        matriz[i][j] = i+1 * j+1;
+                  }
+            }
+
+            //Imprimir tabulado
+            for (int i = 0; i < matriz.length; i++) {
+
+                  System.out.print("|");
+                  for (int j = 0; j < matriz[i].length; j++) {
+                        System.out.print(" "+ matriz[i][j] +" ");
+                  }
+                  System.out.println("|");
+            }
+      }
+
+      public void Extra2(){
+            int matriz[][] = new int[4][4], maiorValor = 0, localizadorL = 0, localizadorC = 0;
+
+            //Dando valor
+            for (int i = 0; i < matriz.length; i++) {
+                  for (int j = 0; j < matriz[i].length; j++) {
+                        matriz[i][j] = rd.nextInt(10);
+                  }
+            }
+
+            //Imprimir tabulado
+            for (int i = 0; i < matriz.length; i++) {
+
+                  System.out.print("|");
+                  for (int j = 0; j < matriz[i].length; j++) {
+                        System.out.print(" "+ matriz[i][j] +" ");
+                  }
+                  System.out.println("|");
+            }
+
+            //Procurando e Imprimindo maior valor
+            for (int i = 0; i < matriz.length; i++) {
+                  for (int j = 0; j < matriz[i].length; j++) {
+                        if(matriz[i][j] >= maiorValor){
+                              maiorValor = matriz[i][j];
+                              localizadorL = i;
+                              localizadorC = j;
+                        }
+                  }
+            }
+
+            System.out.println("O maior valor é: "+ maiorValor +"; Localizada na posição ["+ (localizadorL+1) +"]["+ (localizadorC+1) +"]");
+      }
+
+      public void Extra3(){
+            int matriz[][] = new int[5][5], x, localizadorL = 0, localizadorC = 0;
+
+            //Dando valor; Recebendo x
+            for (int i = 0; i < matriz.length; i++) {
+                  for (int j = 0; j < matriz[i].length; j++) {
+                        matriz[i][j] = rd.nextInt(10);
+                  }
+            }
+
+            System.out.println("Insira um valor para verificar se está na matriz\n[1 - 9]");
+            x = sc.nextInt();
+
+            //Procurando X e Imprimindo sua localização
+            for (int i = 0; i < matriz.length; i++) {
+                  for (int j = 0; j < matriz[i].length; j++) {
+                        if(x == matriz[i][j]){
+                              localizadorL = i;
+                              localizadorC = j;
+                        }
+                  }
+            }
+
+            if(x == 0){
+                  System.out.println("Não encontrado");
+            }
+            else{
+                  System.out.println("X foi encontrado em: ["+ (localizadorL+1) +"]["+ (localizadorC+1) +"]");
+            }
+      }
+
+      public void Extra4(){
+            int matriz[][] = new int[10][10];
+
+            //Dando valor
+            for (int i = 0; i < matriz.length; i++) {
+                  for (int j = 0; j < matriz[i].length; j++) {
+                        if(i < j){
+                              matriz[i][j] = (2*i + 7*j) - 2;
+                        }
+                        else if(i == j){
+                              matriz[i][j] = (int) Math.pow((3*i), 2) - 1;
+                        }
+                        else{
+                              matriz[i][j] =(int) ((Math.pow((4*i), 3) - Math.pow((5*i), 2)) + 1);
+                        }
+                  }
+            }
+
+            //Imprimir tabulado
+            for (int i = 0; i < matriz.length; i++) {
+
+                  System.out.print("|");
+                  for (int j = 0; j < matriz[i].length; j++) {
+                        System.out.print(" "+ matriz[i][j] +" ");
+                  }
+                  System.out.println("|");
+            }
+      }
+
+      public void Extra5(){
+            double matriz[][] = new double[5][4], matriculaMN = 0, localizadorL = 0, localizadorC = 0;
+
+            //Dando valor
+            for (int i = 0; i < matriz.length; i++) {
+                  for (int j = 0; j < matriz[i].length; j++) {
+                        switch (j) {
+                              case 0:
+                                    System.out.println("Insira o número de matrícula.\nApenas números");
+                                    int number = sc.nextInt();
+
+                                    while((int)(number * 10.0) % 10 == 0 ){
+                                          System.out.println("Insira um valor válido\n[Número inteiro]");
+                                          number = sc.nextInt();
+                                    }
+                                    matriz[i][j] = number;
+                                    break;
+
+                              case 1:
+                                    System.out.println("Insira a média das provas.\nApenas números");
+                                    matriz[i][j] = sc.nextDouble();
+                                    break;
+
+                              case 2:
+                                    System.out.println("Insira a média dos trabalhos.\nApenas números");
+                                    matriz[i][j] = sc.nextDouble();
+                                    break;
+
+                              case 3:
+                                    System.out.println("Insira a nota final.\nApenas números");
+                                    matriz[i][j] = matriz[i][j-2] + matriz[i][j-1];
+                                    break;
+                        
+                              default:
+                                    break;
+                        }
+                  }
+            }
+
+            //Imprimindo Resultados
+            //Matrícula do aluno com maior nota
+            for (int i = 0; i < matriz.length; i++) {
+                  for (int j = 0; j < matriz[i].length; j++) {
+                        if(matriz[i][j] >= matriculaMN){
+                              localizadorL = i;
+                              localizadorC = j;
+                        }
+                  }
+            }
+      }
+
+      public void Extra6(){}
+}
