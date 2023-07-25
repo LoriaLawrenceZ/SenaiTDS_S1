7d028b2b3203a9e464f83e7c4348446ebb98d4c3
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-07-25 16:57:21 -0300

    Revisao

---
+++ Java/POO/POORevisao/src/App.java
@@ -0,0 +1,7 @@
+import Cadastro.Animais;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Animais animais = new Animais("João Vicente Silva", "Lata", "Vira-Lata", "Pequeno", 7);
+    }
+}
