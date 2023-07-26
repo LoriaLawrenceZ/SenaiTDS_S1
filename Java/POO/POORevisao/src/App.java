14adea675932f09fdf7b07c865b098034365b9ac
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-07-26 15:18:45 -0300

    Problems with jdk and javafx

--- Java/POO/POORevisao/src/App.java
+++
@@ -1,7 +0,0 @@
-import Cadastro.Animais;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        Animais animais = new Animais("João Vicente Silva", "Lata", "Vira-Lata", "Pequeno", 7);
-    }
-}
